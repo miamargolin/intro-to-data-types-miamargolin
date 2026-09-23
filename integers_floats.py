08d2e40d4961846109ccc25c5796394a3de7372a
miamargolin <miam543@nycstudents.net>
2026-09-23 09:25:34 -0400

    Update integers_floats.py

--- integers_floats.py
+++ integers_floats.py
@@ -1,5 +1,21 @@
-values = [1,2.23,5,7,2,30,15]
+""" values = [1,2.23,5,7,2,30,15]
 print(values)
 for i in values:
     print(i)
 
+x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z) """
+
+sentence = input("Enter your sentence: ")
+print(sentence)
+x = "sentence"
+y= sentence.split( )
+z = y[0]
+print(y)
+print(z)
+
+def count(sentence):
+    
